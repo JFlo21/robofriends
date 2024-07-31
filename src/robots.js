a1a8ee0f5d256fdeac15f57722bca17d9af430e5
JFlo21 <90293985+JFlo21@users.noreply.github.com>
2024-07-31 11:33:13 -0500

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,67 @@
+export const robots =[
+
+	{
+		id: 1, 
+		name: "Jocelinne Romero",
+		username: "jnoemy2018",
+		email: "noemyromero2018@icloud.com"
+		}, 
+
+	{
+		id: 2, 
+		name: "Azrael Alexander Flores",
+		username: "aflo18",
+		email: "aflores2018@yahoo.com"
+
+		},
+
+	{
+		id: 3, 
+		name: "Averly Flores",
+		username: "aveflo12",
+		email: "averlymae2024@gmail.com"
+
+		},
+
+		{
+			id: 4,
+			name: 'Juan Flores',
+			username: 'JFlo21',
+			email: "floresj5400@gmail.com"
+		}, 
+
+		{
+			id: 5,
+			name: "Gregg Hart",
+			username: "ghar14",
+			email: "ghart@gmail.com"
+		},
+
+			{
+			id: 6,
+			name: "Mary Sue",
+			username: "Msue51",
+			email: "Msue51@gmail.com"
+		},
+
+			{
+			id: 7,
+			name: "Anthony Dark",
+			username: "aDark15",
+			email: "aDark15@gmail.com"
+		}
+]
+
+export default robots
+
+
+
+
+
+
+
+
+
+
+
+
